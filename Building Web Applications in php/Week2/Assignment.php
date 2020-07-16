1055dbde57c644594a7840f9009c859fcab8791c
Shivam Pal <56273423+spleon12@users.noreply.github.com>
2020-07-16 20:44:33 +0530

    Create Assignment.php

---
+++ Building Web Applications in php/Week2/Assignment.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html>
+<head>
+<title>
+the title from Auto Grader</title>
+<meta charset="UTF-8">
+</head>
+<body>
+<div>
+<h1><strong> <span style="color:red"><h2/>Hi</span><br> By Shivam </strong></h1>
+</div>
+<strong>Your Name </strong><br>
+ <a href="https://www.google.com">Visit google.com!</a>
+<ul>
+<li><h3> Anything</h3></li>
+</ul>
+</body>
+</html>
